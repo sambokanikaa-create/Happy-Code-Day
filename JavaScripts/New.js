043d87ee606f89e1e9c6acb9ebddb8496fc0d67b
sambokanikaa <sambokanikaa@gmail.com>
2026-06-21 15:11:35 +0700

    first push

---
+++ JavaScripts/New.js
@@ -0,0 +1,20 @@
+console.log("Hello JavaScripts");
+console.log("I'm not good!");
+
+// This one was name in c++
+let variable=10; 
+// This one like cout<<
+console.log(variable);
+
+let name="Sambo Kanika";
+let age=19;
+let gender="Female";
+let email="sambokanikaa@gmail.com";
+
+// This one like cout<<"Name : "<<name;
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("Gender : " + gender);
+console.log("Email : " + email);
+
+// Javascripts don't have like cin to input, we use html to input in
