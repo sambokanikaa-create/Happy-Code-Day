f3c5201ab3aa73a1d4bb144e3a21d44fd6b1ac67
sambokanikaa <sambokanikaa@gmail.com>
2026-06-23 08:47:48 +0700

    third push

---
+++ JavaScripts/Exercise.js
@@ -0,0 +1,50 @@
+let w=100;
+let h=50;
+console.log("Width : ", w);
+console.log("Height : ", h);
+console.log("Surrounding : ", (w+h)*2);
+console.log("Area : ",w*h);
+console.log("\n");
+
+let s1=10;
+let s2=20;
+let s3=30;
+let s4=40;
+let s5=50;
+let total= s1+s2+s3+s4+s5;
+console.log("Score1: ", s1);
+console.log("Score2: ", s2);
+console.log("Score3: ", s3);
+console.log("Score4: ", s4);
+console.log("Score5: ", s5);
+console.log("Total: ", total);
+console.log("Average: ", total/5);
+console.log("\n")
+
+let code=008;
+let name="Cherry Lipstick";
+let qty=100;
+let price=10;
+let discount=20;
+
+let allPrice=price*100;
+let discountPrice=allPrice*discount/100;
+
+console.log("Code: ", code);
+console.log("Name: ", name);
+console.log("Quantity: ", qty);
+console.log("Price: ", price);
+console.log("Discount: ", discount);
+console.log("Discount Price: ", discountPrice, "$");
+console.log("Total: ", allPrice, "$");
+console.log("Payment: ", allPrice-discountPrice, "$");
+console.log("\n");
+
+let principle=1000;
+let rate=5;
+let time=2;
+let interest=(principle*rate*time)/100;
+console.log("Principle: ", principle);
+console.log("Rate: ", rate);
+console.log("Time: ", time);
+console.log("Total Interest: ", interest, "$");
