45b18a7a195b353d234d2f8b151c173a1da078d7
sambokanikaa <sambokanikaa@gmail.com>
2026-06-27 16:30:48 +0700

    First push

---
+++ JavaScripts/Nest if (if , if ).js
@@ -0,0 +1,16 @@
+let age =18
+
+if(age>=18){
+    console.log("You're adult.")
+} 
+else if(age<18){
+    console.log("You're teenager.\n")
+}
+
+console.log("=========================")
+if(age>=18){
+    console.log("You're adult.")
+} 
+if(age<18){
+    console.log("You're teenager.")
+}
